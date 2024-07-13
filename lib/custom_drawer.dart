7120127fcb4a05cd4c408259cde1bf1bcc8da7c5
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2024-07-13 13:38:53 +0600

    complete

--- lib/custom_drawer.dart
+++ lib/custom_drawer.dart
@@ -1,4 +1,5 @@
-import 'package:complex_ui/main.dart';
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 class CustomDrawer extends StatefulWidget {
@@ -10,59 +11,89 @@ class CustomDrawer extends StatefulWidget {
 
 class _CustomDrawerState extends State<CustomDrawer>
     with SingleTickerProviderStateMixin {
-  static const Duration toggleDuration = Duration(milliseconds: 250);
-  static const double maxSlide = 100.0;
-  static const double minDragStartEdge = 60;
-  static const double maxDragStartEdge = maxSlide - 16;
-  late AnimationController _animationController;
+  late AnimationController animationController;
   bool _canBeDragged = false;
+  final double maxSlide = 300.0;
 
   @override
   void initState() {
-    _animationController = AnimationController(
-      vsync: this,
-      duration: toggleDuration,
-    );
     super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+    );
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
+    animationController.dispose();
     super.dispose();
   }
 
-  void close() => _animationController.reverse();
-
-  void open() => _animationController.forward();
-
-  void toggleDrawer() => _animationController.isCompleted ? close() : open();
+  void toggle() => animationController.isDismissed
+      ? animationController.forward()
+      : animationController.reverse();
 
   @override
   Widget build(BuildContext context) {
-    var myChild = const MyHomePage(title: "");
     return GestureDetector(
       onHorizontalDragStart: _onDragStart,
       onHorizontalDragUpdate: _onDragUpdate,
       onHorizontalDragEnd: _onDragEnd,
-      onTap: toggleDrawer,
+      behavior: HitTestBehavior.translucent,
+      onTap: toggle,
       child: AnimatedBuilder(
-        animation: _animationController,
+        animation: animationController,
         builder: (context, _) {
-          double animValue = _animationController.value;
-          final slideAmount = maxSlide * animValue;
-          final contentScale = 1.0 - (0.3 * animValue);
-          return Stack(
-            children: [
-              const MyDrawer(),
-              Transform(
-                transform: Matrix4.identity()
-                  ..translate(slideAmount)
-                  ..scale(contentScale, contentScale),
-                alignment: Alignment.centerRight,
-                child: myChild,
-              ),
-            ],
+          return Material(
+            color: Colors.blueGrey,
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(maxSlide * (animationController.value - 1), 0),
+                  child: Transform(
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.001)
+                      ..rotateY(math.pi / 2 * (1 - animationController.value)),
+                    alignment: Alignment.centerRight,
+                    child: const MyDrawer(),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(maxSlide * animationController.value, 0),
+                  child: Transform(
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.001)
+                      ..rotateY(-math.pi * animationController.value / 2),
+                    alignment: Alignment.centerLeft,
+                    child: const MyHomePage(title: ''),
+                  ),
+                ),
+                Positioned(
+                  top: 4.0 + MediaQuery.of(context).padding.top,
+                  left: 4.0 + animationController.value * maxSlide,
+                  child: IconButton(
+                    icon: const Icon(Icons.menu),
+                    onPressed: toggle,
+                    color: Colors.white,
+                  ),
+                ),
+                Positioned(
+                  top: 16.0 + MediaQuery.of(context).padding.top,
+                  left: animationController.value *
+                      MediaQuery.of(context).size.width,
+                  width: MediaQuery.of(context).size.width,
+                  child: Opacity(
+                    opacity: 1 - animationController.value,
+                    child: Text(
+                      'Hello Flutter Europe',
+                      style: Theme.of(context).primaryTextTheme.titleLarge,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           );
         },
       ),
@@ -70,18 +101,15 @@ class _CustomDrawerState extends State<CustomDrawer>
   }
 
   void _onDragStart(DragStartDetails details) {
-    bool isDragOpenFromLeft = _animationController.isDismissed &&
-        details.globalPosition.dx < minDragStartEdge;
-    bool isDragCloseFromRight = _animationController.isCompleted &&
-        details.globalPosition.dx > maxDragStartEdge;
-
+    bool isDragOpenFromLeft = animationController.isDismissed;
+    bool isDragCloseFromRight = animationController.isCompleted;
     _canBeDragged = isDragOpenFromLeft || isDragCloseFromRight;
   }
 
   void _onDragUpdate(DragUpdateDetails details) {
     if (_canBeDragged) {
-      double delta = details.primaryDelta ?? 0 / maxSlide;
-      _animationController.value += delta;
+      double delta = details.primaryDelta! / maxSlide;
+      animationController.value += delta;
     }
   }
 
@@ -89,18 +117,18 @@ class _CustomDrawerState extends State<CustomDrawer>
     //I have no idea what it means, copied from Drawer
     double kMinFlingVelocity = 365.0;
 
-    if (_animationController.isDismissed || _animationController.isCompleted) {
+    if (animationController.isDismissed || animationController.isCompleted) {
       return;
     }
     if (details.velocity.pixelsPerSecond.dx.abs() >= kMinFlingVelocity) {
       double visualVelocity = details.velocity.pixelsPerSecond.dx /
           MediaQuery.of(context).size.width;
 
-      _animationController.fling(velocity: visualVelocity);
-    } else if (_animationController.value < 0.5) {
-      close();
+      animationController.fling(velocity: visualVelocity);
+    } else if (animationController.value < 0.5) {
+      animationController.reverse();
     } else {
-      open();
+      animationController.forward();
     }
   }
 }
@@ -110,42 +138,94 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.blueAccent,
-      child: SafeArea(
-        child: Theme(
-          data: ThemeData(brightness: Brightness.dark),
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Icon(
-                Icons.agriculture_outlined,
-                size: 200,
-              ),
-              ListTile(
-                leading: Icon(Icons.new_releases),
-                title: Text('News'),
-              ),
-              ListTile(
-                leading: Icon(Icons.star),
-                title: Text('Favourites'),
-              ),
-              ListTile(
-                leading: Icon(Icons.map),
-                title: Text('Map'),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-              ),
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text('Profile'),
-              ),
-            ],
+    return SizedBox(
+      width: 300,
+      height: double.infinity,
+      child: Material(
+        color: Colors.blueAccent,
+        child: SafeArea(
+          child: Theme(
+            data: ThemeData(brightness: Brightness.dark),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Icon(
+                  Icons.agriculture_outlined,
+                  size: 200,
+                ),
+                ListTile(
+                  leading: Icon(Icons.new_releases),
+                  title: Text('News'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.star),
+                  title: Text('Favourites'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.map),
+                  title: Text('Map'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Settings'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Profile'),
+                ),
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
